67b08cd80a197e85c18c36654f056cdddbd5a3a6
NoriakiNakamura <babababargxx@gmail.com>
2022-01-27 09:24:07 +0000

    nameでloginの設定fin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+  #ストングパラメータ
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+  #サインアップが行われる前に名前の受付を許可
+  
 end
