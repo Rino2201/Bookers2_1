b85a945b5aa718c11edc41553216a571a7e738f8
NoriakiNakamura <babababargxx@gmail.com>
2022-01-28 09:39:30 +0000

    Bootstrapまで

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'users/index'
-  get 'users/show'
-  get 'users/edit'
-  get 'books/new'
-  get 'books/index'
-  get 'books/show'
   devise_for :users
 
   root 'homes#top'
