44ddeb6e869920160cd27227f50e77e8ff73ce55
NoriakiNakamura <babababargxx@gmail.com>
2022-01-28 09:19:34 +0000

    Activate_storageの導入,アソシエーションの設定,モデル・コントローラの作成,resourcesでのルーティング

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,17 @@
 class ApplicationController < ActionController::Base
-  
+
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_in_path_for(resource)
+    about_path
+  end
+  #ログイン後→"/users/id(users_show)"へ
+
+
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+  #ログアウト後→"/" "homes#top"へ
 
   protected
   #ストングパラメータ
@@ -8,5 +19,5 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
   #サインアップが行われる前に名前の受付を許可
-  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,19 @@
 Rails.application.routes.draw do
-  
+
+  get 'users/index'
+  get 'users/show'
+  get 'users/edit'
+  get 'books/new'
+  get 'books/index'
+  get 'books/show'
   devise_for :users
 
   root 'homes#top'
   get '/home/about'=>'homes#about',as:"about"
-  
+
+  resources :users, only: [:new, :index, :show]
+
+  resources :books, only: [:index, :show, :edit]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
